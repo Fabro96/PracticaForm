18cc6b3cb369f5ddf9ca5b3e78002585ba0322ba
Fabrizio Agustín Ghilardini <70611279+Fabro96@users.noreply.github.com>
2020-10-23 19:03:36 -0300

    Commit
    .

---
+++ PracticaForms.Presentacion/PracticaForms.Negocio/Jugador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaForms.Negocio
+{
+    public class Jugador
+    {
+        private int _codigo;
+        private string _nombre;
+        private int _edad;
+        private int _distancia;
+
+        public Jugador() { }
+
+        public Jugador(int codigo, int edad, int distancia, string nombre)
+        {
+            this._codigo = codigo;
+            this._nombre = nombre;
+            this._edad = edad;
+            this._distancia = distancia;
+        }
+
+        public int Codigo
+        {
+            set { _codigo = value; }
+            get { return _codigo;  }
+        }
+        public string Nombre
+        {
+            set { _nombre = value; }
+            get { return _nombre;  }
+        }
+        public int Edad
+        {
+            set { _edad = value; }
+            get { return _edad;  }
+        }
+        public int Distancia
+        {
+            set { _distancia = value; }
+            get { return _distancia;  }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Jugador {0} Nombre {1} Edad {2} Distancia {3}", this.Codigo, this.Nombre, this.Edad.ToString(), this.Distancia);
+        }
+    }
+}
---
+++ PracticaForms.Presentacion/PracticaForms.Negocio/JugadorBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaForms.Negocio
+{
+    public class JugadorBL
+    {
+        private List<Jugador> _listaJugadores;
+
+        public JugadorBL()
+        {
+            _listaJugadores = new List<Jugador>();
+
+            _listaJugadores.Add(new Jugador(1, 29, 19, "Juan Gonzalez"));
+            _listaJugadores.Add(new Jugador(2, 22, 12, "Claudia Owen"));
+
+        }
+        
+        public List<Jugador> GetAll()
+        {
+            return _listaJugadores;
+        }
+        public void Insert(Jugador jugador)
+        {
+            _listaJugadores.Add(jugador);
+        }
+    }
+}
